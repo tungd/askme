adeb65a78c3cf8657172ac805b1aeb7a0fc8eade
Tung Dao <me@tungdao.com>
2018-05-25 13:21:49 +0700

    index view

--- askme/asking/views.py
+++ askme/asking/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from . import models
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    queryset = models.Question.objects.all()
