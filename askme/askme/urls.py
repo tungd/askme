b34ea224bb48864775cc75eab60332948874edb4
Tung Dao <me@tungdao.com>
2018-05-28 11:31:24 +0700

    setup static assets

--- askme/askme/urls.py
+++ askme/askme/urls.py
@@ -1,10 +1,21 @@
 import asking.views as asking_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('<str:slug>', asking_views.ProfileView.as_view(),
+    path('',
+         asking_views.IndexView.as_view(),
+         name='index'),
+    path('questions/<int:pk>',
+         asking_views.UpdateQuestionView.as_view(),
+         name='answer'),
+    path('questions',
+         asking_views.CreateQuestionView.as_view(),
+         name='ask'),
+    path('<str:slug>',
+         asking_views.ProfileView.as_view(),
          name='profile'),
-    path('', asking_views.IndexView.as_view(), name='index')
 ]
